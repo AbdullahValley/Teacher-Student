ef534cb71fefae4b883273101f9c55962d6d08ff
Abdullah <noman19bd@yahoo.com>
2019-03-20 21:14:47 +0600

    Welcome Design Changed into New Theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,60 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Teachers & Students Portal</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+    <!-- Bootstrap CSS File -->
+    <link href="{{ asset('theme_welcome/lib/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+    <!-- Libraries CSS Files -->
+    <link href="{{ asset('theme_welcome/lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme_welcome/lib/animate-css/animate.min.css') }}" rel="stylesheet">
 
-        .full-height {
-            height: 100vh;
-        }
+    <!-- Main Stylesheet File -->
+    <link href="{{ asset('theme_welcome/css/style.css') }}" rel="stylesheet">
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
 
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
 </head>
+
 <body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+<div id="preloader"></div>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
+<section id="hero">
+    <div class="hero-container">
+        <div class="wow fadeIn">
+            <div class="hero-logo">
+                <img class="" src="{{ asset('theme_welcome/img/logo.png') }}" alt="Teacher's Portal">
+            </div>
+
+            <h1>Welcome to Teacher's Portal</h1>
+            <h2>You can <span class="rotating">view routines, view counseling schedule, update profile</span></h2>
+            <div class="actions">
+                <a href="{{ route('login') }}" class="btn-get-started">LOGIN</a>
+                <a href="{{ route('register') }}" class="btn-services">REGISTER</a>
+            </div>
         </div>
-    @endif
-
-    <div class="content">
-        <div class="title m-b-md">
-            Teachers & Students Portal
-        </div>
-
     </div>
-</div>
+</section>
+
+
+<!-- Required JavaScript Libraries -->
+<script src="{{ asset('theme_welcome/lib/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/superfish/hoverIntent.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/superfish/superfish.min.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/morphext/morphext.min.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/wow/wow.min.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/stickyjs/sticky.js') }}"></script>
+<script src="{{ asset('theme_welcome/lib/easing/easing.js') }}"></script>
+
+<!-- Template Specisifc Custom Javascript File -->
+<script src="{{ asset('theme_welcome/js/custom.js') }}"></script>
+
 </body>
+
 </html>
